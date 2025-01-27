c212806d1a758c4b82dd4174d1d276c1558d76e2
unknown <lrl0015@alu.medac.es>
2025-01-27 09:36:21 +0100

    criteria added on EditPlayerScreen

--- app/src/main/java/com/wito/raidscorerapp/model/Player.kt
+++ app/src/main/java/com/wito/raidscorerapp/model/Player.kt
@@ -3,6 +3,27 @@ package com.wito.raidscorerapp.model
 data class Player (
     val nombre : String,
     val clase : String,
-    val especializacion : String
-)
+    val especializacion : String,
+    //Criterios
+    var puntualidad : Int = 0,
+    var mecanicas : Int = 0,
+    var consumibles : Int = 0,
+    var actitud : Int = 0
+) {
+    private val pesos = mapOf(
+        "puntualidad" to 0.25,
+        "mecanicas" to 0.3,
+        "consumibles" to 0.2,
+        "actitud" to 0.2
+    )
+
+    fun calcularPuntuacionFinal(): Double {
+        return (
+                puntualidad * pesos["puntualidad"]!! +
+                mecanicas * pesos["mecanicas"]!! +
+                consumibles * pesos["consumibles"]!! +
+                actitud * pesos["actitud"]!!
+                )
+    }
+}
 
--- app/src/main/java/com/wito/raidscorerapp/screens/EditPlayerScreen.kt
+++ app/src/main/java/com/wito/raidscorerapp/screens/EditPlayerScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.room.util.copy
 import com.wito.raidscorerapp.model.Player
 
 
@@ -28,11 +30,17 @@ fun EditPlayerScreen(
     var clase by remember { mutableStateOf(player.clase) }
     var especializacion by remember { mutableStateOf(player.especializacion) }
 
+    //Criteria
+    var puntualidad by remember { mutableStateOf(player.puntualidad.toFloat()) }
+    var mecanicas by remember { mutableStateOf(player.mecanicas.toFloat()) }
+    var consumibles by remember { mutableStateOf(player.consumibles.toFloat()) }
+    var actitud by remember { mutableStateOf(player.actitud.toFloat()) }
+
     Column (
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
     ) {
         Text(
             text = "Editar jugador",
@@ -61,9 +69,42 @@ fun EditPlayerScreen(
             modifier = Modifier.fillMaxWidth()
         )
 
+        //Slider criteria selection
+        CriterioSlider(
+            label = "Puntualidad",
+            value = puntualidad,
+            onValueChange = {puntualidad = it}
+        )
+
+        CriterioSlider(
+            label = "Mecanicas",
+            value = mecanicas,
+            onValueChange = {mecanicas = it}
+        )
+
+        CriterioSlider(
+            label = "Consumibles",
+            value = consumibles,
+            onValueChange = {consumibles = it}
+        )
+
+        CriterioSlider(
+            label = "Actitud",
+            value = actitud,
+            onValueChange = {actitud = it}
+        )
+
         Button(
             onClick = {
-                val updatedPlayer = Player(nombre, clase, especializacion)
+                val updatedPlayer = player.copy(
+                    nombre = nombre,
+                    clase = clase,
+                    especializacion = especializacion,
+                    puntualidad = puntualidad.toInt(),
+                    mecanicas = mecanicas.toInt(),
+                    consumibles = consumibles.toInt(),
+                    actitud = actitud.toInt()
+                    )
                 onEditPlayer(updatedPlayer)
             },
             modifier = Modifier.fillMaxWidth()
@@ -77,5 +118,26 @@ fun EditPlayerScreen(
         ){
             Text("Cancelar")
         }
+    }
+}
+
+@Composable
+fun CriterioSlider(
+    label : String,
+    value : Float,
+    onValueChange: (Float) -> Unit
+){
+    Column (modifier = Modifier.fillMaxWidth()){
+        Text(
+            text = "$label: ${value.toInt()}",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Slider (
+            value = value,
+            onValueChange = onValueChange,
+            valueRange = 1f..3f,
+            steps = 1,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
     }
 }

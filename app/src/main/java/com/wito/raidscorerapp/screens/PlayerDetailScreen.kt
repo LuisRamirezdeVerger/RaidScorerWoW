245e2d4620c20a21a29606f5e3fc79b06d666caa
unknown <lrl0015@alu.medac.es>
2025-01-27 10:18:10 +0100

    File PlayerDetailScreen added

---
+++ app/src/main/java/com/wito/raidscorerapp/screens/PlayerDetailScreen.kt
@@ -0,0 +1,61 @@
+package com.wito.raidscorerapp.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.wito.raidscorerapp.model.Player
+
+
+@Composable
+fun PlayerDetailScreen(
+    player : Player,
+    onBack: () -> Unit
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Text(
+            text = "Detalles de ${player.nombre}",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        //Individual criteria
+        Text(
+            text = "Puntualidad: ${player.puntualidad}",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "Mecánicas: ${player.mecanicas}",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "Consumibles: ${player.consumibles}",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "Actitud: ${player.actitud}",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        //Back Button
+        Button(
+            onClick = {onBack},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Volver")
+        }
+    }
+}

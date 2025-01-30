f0d1dac5fe32377384687de540cc926a68aee396
Luis a.k.a. Wito <lrl0015@alu.medac.es>
2025-01-30 22:18:24 +0100

    ScoreWeights object finished. RaidSession class added

--- app/src/main/java/com/wito/raidscorerapp/model/Player.kt
+++ app/src/main/java/com/wito/raidscorerapp/model/Player.kt
@@ -3,28 +3,28 @@ package com.wito.raidscorerapp.model
 import androidx.compose.material3.DividerDefaults.color
 
 data class Player (
-    val nombre : String,
-    val clase : String,
-    val especializacion : String,
+    val name : String,
+    val classWoW : String,
+    val specialization : String,
     //Criterios
-    var puntualidad : Int = 0,
-    var mecanicas : Int = 0,
-    var consumibles : Int = 0,
-    var actitud : Int = 0
+    var punctuality : Int = 0,
+    var mechanics : Int = 0,
+    var consumables : Int = 0,
+    var attitude : Int = 0
 ) {
-    private val pesos = mapOf(
-        "puntualidad" to 2.5,
-        "mecanicas" to 3.0,
-        "consumibles" to 2.0,
-        "actitud" to 2.0
-    )
+    //private val pesos = mapOf(
+    //    "puntualidad" to 2.5,
+    //    "mecanicas" to 3.0,
+    //    "consumibles" to 2.0,
+    //    "actitud" to 2.0
+    //)
 
-    fun calcularPuntuacionFinal(): Double {
+    fun calculatFinalScore(): Double {
         return (
-                puntualidad * pesos["puntualidad"]!! +
-                mecanicas * pesos["mecanicas"]!! +
-                consumibles * pesos["consumibles"]!! +
-                actitud * pesos["actitud"]!!
+                punctuality * ScoreWeights.punctuality +
+                        mechanics * ScoreWeights.mechanics +
+                        consumables * ScoreWeights.consumables +
+                        attitude * ScoreWeights.attitude
                 )
     }
 }
---
+++ app/src/main/java/com/wito/raidscorerapp/model/RaidSession.kt
@@ -0,0 +1,4 @@
+package com.wito.raidscorerapp.model
+
+class RaidSession {
+}

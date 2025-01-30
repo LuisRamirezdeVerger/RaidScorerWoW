a86a40f646e044b7345f90fa7af83364a5409fd1
Luis a.k.a. Wito <lrl0015@alu.medac.es>
2025-01-30 22:14:12 +0100

    ScoreWeights object added

---
+++ app/src/main/java/com/wito/raidscorerapp/model/ScoreWeights.kt
@@ -0,0 +1,8 @@
+package com.wito.raidscorerapp.model
+
+object ScoreWeights {
+    var punctuality: Double = 2.5
+    var mechanics: Double = 3.0
+    var consumables: Double = 1.0
+    var attitude: Double = 1.5
+}
